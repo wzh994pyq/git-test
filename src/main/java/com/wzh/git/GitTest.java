2dbe9f95f47b2bd7756a3f28531582958c2b631e
wzh <wzh@wzhuoh.com>
2022-06-02 16:07:34 +0800

    first commit

---
+++ src/main/java/com/wzh/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.wzh.git;
+
+/**
+ * @author shkstart
+ * @create 2022-06-02  16:00
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
